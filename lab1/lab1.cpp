184095872845cf2ed6b3b36ac2b722c2bdd45ed0
Nick Conant <nconant001@student.butte.edu>
2016-08-25 20:51:48 +0000

    Lab 1 folder updated for ASCII art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,15 @@
+//Nick Conant
+//25 Aug 2016
+//ASCII Art
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " _A_A_"  << endl;
+    cout << "| @ @ |" << endl;
+    cout << "|  ^  |" << endl;
+    cout << "| ~~~ |" << endl;
+    cout << " V---V " << endl;
+}
