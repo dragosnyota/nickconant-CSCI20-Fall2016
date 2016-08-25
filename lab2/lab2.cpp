3826015dd2864939c014fd89db409b2fabb39bae
Nick Conant <nconant001@student.butte.edu>
2016-08-25 21:11:58 +0000

    lab 2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,15 @@
+//Nick Conant
+//25 Aug 2016
+//Number guessing algorithm
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Step 1: Computer chooses a random number between 0 and 100
+    //Step 2: User is asked to guess a number between said numbers
+    //Step 3: If user guesses correct number, congratulate user and add 10 points
+    //Step 4: If user guess is incorect subract 1 point from 10 for each incorrect guess and state whether actual number is higher or lower
+    //Step 5: If user does not get the number correct after 10 tries, reveal correct number and start over from step one
+}
