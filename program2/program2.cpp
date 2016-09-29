aacdcc8e5e8d778a5b8b2332ed6303183aeb2532
Nick Conant <nconant001@student.butte.edu>
2016-09-29 22:37:55 +0000

    distance calculator submission

---
+++ program2/program2.cpp
@@ -0,0 +1,118 @@
+// Created by: Nick Conant
+// Last Modified on: 29 Sep 2016
+// Program 2: Distance Converter
+
+#include <iostream>
+using namespace std;
+
+//Class for converting distances including: miles, yards, feet and inches
+class DistanceConverter {
+    
+    public:
+    
+    double yardsVal = 0.0;
+    double metersVal = 0.0;
+    double feetVal = 0.0;
+    double inchesVal = 0.0;
+    
+    void SetDistFromMiles(double miles_);
+    void SetDistFromYards(double yardsVal);
+    void SetDistFromFeet(double feetVal);
+    void SetDistFromInches(double inchesVal);
+    void SetDistFromMeters(double metersVal);
+    double GetDistAsMiles();
+    double GetDistAsYards();
+    double GetDistAsMeters();
+    double GetDistAsFeet();
+    double GetDistAsInches();
+    void PrintDist();
+    
+    DistanceConverter();              // default constructor
+    DistanceConverter(double miles_); // second constructor
+    
+    private:
+    
+    double miles_ = 0.0;
+    
+};
+
+// default constructur
+DistanceConverter::DistanceConverter() {
+    miles_ = 0.0;
+    
+    return;
+}
+
+//second constructor
+DistanceConverter::DistanceConverter(double miles_) {
+    miles_ = 0.0;
+    
+    return;
+}
+//sets miles_
+void DistanceConverter::SetDistFromMiles(double mileVal) {
+    miles_ = mileVal;
+    return;
+}
+//returns miles_ value
+double DistanceConverter::GetDistAsMiles() {
+    return miles_;
+}
+//converts yards to miles
+void DistanceConverter::SetDistFromYards(double yardsVal) {
+    miles_ = yardsVal * 0.00056818;
+    
+    return;
+}
+//converts feet to miles
+void DistanceConverter::SetDistFromFeet(double feetVal) {
+    miles_ = feetVal * 0.00018939;
+    
+    return;
+}
+//converts inches to miles
+void DistanceConverter::SetDistFromInches(double inchesVal) {
+    miles_ = (inchesVal / 12.0) * 0.00018939;
+    
+    return;
+}
+//converts meters to miles
+void DistanceConverter::SetDistFromMeters(double metersVal) {
+    miles_ = metersVal * 0.00062137;
+    
+    return;
+}
+//converts miles to meters
+double DistanceConverter::GetDistAsMeters() {
+    metersVal = miles_ / 0.00062137;
+    
+    return metersVal;
+}
+//converts miles to yards
+double DistanceConverter::GetDistAsYards() {
+    yardsVal = miles_ / 0.00056818;
+    
+    return yardsVal;
+}
+//converts miles to feet
+double DistanceConverter::GetDistAsFeet() {
+    feetVal = miles_ / 0.00018939;
+    
+    return feetVal;
+}
+//converts miles to inches
+double DistanceConverter::GetDistAsInches() {
+    inchesVal = (miles_ / 0.00018939) * 12;
+    
+    return inchesVal;
+}
+//prints distance values
+void DistanceConverter::PrintDist() {
+    cout << "Miles: " << miles_ << endl;
+    cout << "Meters: " << metersVal << endl;
+    cout << "Yards: " << yardsVal << endl;
+    cout << "Feet: " << feetVal << endl;
+    cout << "Inches: " << inchesVal << endl << endl;
+    
+    return;
+}
---
+++ program2/program2main.cpp
@@ -0,0 +1,47 @@
+//main for running program2.cpp
+#include <iostream>
+#include "program2.cpp"
+using namespace std;
+
+int main() {
+    
+    DistanceConverter dist1;
+    DistanceConverter dist2;
+    DistanceConverter dist3;
+    DistanceConverter dist4;
+    DistanceConverter dist5;
+    DistanceConverter dist6;
+    DistanceConverter dist7;
+    DistanceConverter dist8;
+    
+    dist1.SetDistFromMiles(1);
+    dist1.GetDistAsFeet();
+    dist1.PrintDist();
+    
+    dist2.SetDistFromMiles(1);
+    dist2.GetDistAsInches();
+    dist2.PrintDist();
+    
+    dist3.SetDistFromMiles(1);
+    dist3.GetDistAsMeters();
+    dist3.PrintDist();
+    
+    dist4.SetDistFromMiles(1);
+    dist4.GetDistAsYards();
+    dist4.PrintDist();
+    
+    dist5.SetDistFromMeters(16.0);
+    dist5.PrintDist();
+    
+    dist6.SetDistFromYards(20.0);
+    dist6.GetDistAsFeet();
+    dist6.PrintDist();
+    
+    dist7.SetDistFromInches(100.0);
+    dist7.GetDistAsMeters();
+    dist7.PrintDist();
+    
+    dist8.SetDistFromMeters(1.0);
+    dist8.PrintDist();
+    
+}
